43dd06d1497e15e5adef2670e29d8dfcf85d91d1
S-Kirua26 <steevengheeraert26@gmail.com>
2020-09-15 09:01:56 +0200

    Version1

--- PHP/Exo2.3.php
+++ PHP/Exo2.3.php
@@ -1,5 +1,8 @@
 <?php
-    $prix = readline("Entrez le prix HT de l'article");
+    $prix = readline("Entrez le prix HT d'un article");
     $nombre = readline("Veuillez entrez le nombre d'articles");
-    
+    $tva = readline("Veuillez entrez le taux de TVA");
+
+    $resultat = $prix*$nombre*$tva;
+    echo $resultat;
 ?>
