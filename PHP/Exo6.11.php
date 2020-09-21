1c7786424db91dd12a39ee46c994e71a9ca8c7aa
S-Kirua26 <steevengheeraert26@gmail.com>
2020-09-21 17:05:03 +0200

    Version0

--- PHP/Exo6.11.php
+++ PHP/Exo6.11.php
@@ -1,3 +1,30 @@
 <?php
 
+require "LesFonctions.php";
+
+$demande = demanderEntier2("Combien de valeurs max aura votre 1er tableau?");
+$tableau = creerTableauTaille($demande);
+afficherTableauForeach($tableau);
+$demande2 = demanderEntier2("Combien de valeurs aura votre second tableau?");
+$tableau2 = creerTableauTaille($demande2);
+afficherTableauForeach($tableau2);
+
+    /*
+    for ($i = 0; $i < $demande2; $i++)
+    {
+          for ($j = 0; $j < $demande; $j++)
+          {
+             $calcul[] = $tableau2[$i] * $tableau[$j]; 
+          }
+    }
+    */
+
+    for ($i = 0; $i < $demande; $i++)
+    {
+          for ($j = 0; $j < $demande2; $j++)
+          {
+             $calcul[] = $tableau2[$j] * $tableau[$i]; 
+          }
+    }
+echo array_sum($calcul);
 ?>
