a966fbddac33e50795f879235d3a11eded32f858
S-Kirua26 <steevengheeraert26@gmail.com>
2020-09-22 17:19:11 +0200

    Version0

--- PHP/Exo7.1.php
+++ PHP/Exo7.1.php
@@ -8,16 +8,48 @@ $demande = demanderEntier2("Combien de valeurs il y aura dans le tableau?");
 $tableau = creerTableauTaille($demande);
 afficherTableauForeach($tableau);
 
+$consecutif = true;
+$i = 0;
 
+if ($tableau[0] < $tableau[$i + 1])
+{
+    $sens = 1;
+}
+else
+{
+    $sens = 0;
+}
 
-if ($resultat = 1)
+do
+{
+    $consecutif = false;
+    if ($sens == 1)
+    {
+        if ($tableau[$i] + 1 == $tableau[$i+1])
+        {
+            $consecutif = true;
+            $i+=1;
+        }
+    }
+    else
+    {
+        if ($tableau[$i] - 1 == $tableau[$i+1])
+        {
+            $consecutif = true;
+            $i+=1;
+        }
+    }
+
+} while ($consecutif && $i < count($tableau) - 1);
+
+if ($consecutif == true)
 {
     echo "Les valeurs sont consecutif";
 }
 else
 {
-    echo "Les valeurs ne sont pas consécutif";
+    echo "Les valeurs ne sont pas consecutif";
 }
 
-
+// Je remercie Marvin pour ce code
 ?>
