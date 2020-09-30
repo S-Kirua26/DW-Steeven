690c39d01fc5543f6c1f963a884b366b603b135f
59011-95-06 <59011-95-06@exchange.ad.afpanet>
2020-09-30 15:24:35 +0200

    Version0

--- PHP/tableaux2D/Morpion2.0.php
+++ PHP/tableaux2D/Morpion2.0.php
@@ -10,18 +10,20 @@ function afficherTableau($tableau)
 
 function choixSymbole($nbJoueur)
 {
-    $symboles = [];
+    $symboles = []; // initialisation du tableau $symboles (qui contiendra les symboles des joueurs), pour l'instant vide
     for ($i = 0; $i < $nbJoueur; $i++) {
 
         do // on va verifier les données des joueurs
         {
            $symbole = $demande = strtoupper(readline("Quel est votre symbole joueur " . $i . " ? ")); // pour chaque joueur, on demande le symbole
     
-            if ((!ctype_alnum($demande)) or (strlen($demande) > 1)) 
+            if ((!ctype_alnum($demande)) or (strlen($demande) > 1) or (in_array($symbole,$symboles) === true)) 
             {
                 echo "Saisie incorrect, veuillez recommencer \n";
             }
-        }while ((!ctype_alnum($demande)) or (strlen($demande) > 1) or (in_array($symbole,$symboles) === true));
+        }
+        // on boucle tant que le symbole n'est pas alphanumérique, que le symbole fait plus de un caractère et que le symbole est déjà utilisés(choisi)
+        while ((!ctype_alnum($demande)) or (strlen($demande) > 1) or (in_array($symbole,$symboles) === true)); 
 
          $symboles[$i] = $symbole; // dans le tableau $symboles, on ajoute les symboles des joueurs
     }
