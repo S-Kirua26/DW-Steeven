d1ce778f49d2da735f40eb7267840a52745fd30e
59011-95-06 <59011-95-06@exchange.ad.afpanet>
2020-10-06 10:23:10 +0200

    Version0

--- PHP/Orientee Objet/ClassEmploye.php
+++ PHP/Orientee Objet/ClassEmploye.php
@@ -2,6 +2,7 @@
 
 class Employe
 {
+    // on commence par créer les variables dont on aura besoin
     private $_nom;
     private $_prenom;
     private $_datembauche;
@@ -34,42 +35,36 @@ class Employe
     {
         return $this->_service;
     }
-    public function getDate()
-    {
-        return $this->_date;
-    }
+
 
     // on initialise les assesseurs setteurs
     public function setNom($nom)
     {
-        $this->_nom = ($nom);
+        $this->_nom = $nom;
     }
     public function setPrenom($prenom)
     {
-        $this->_prenom = ($prenom);
+        $this->_prenom = $prenom;
     }
-    public function setDatembauche($datembauche)
+    public function setDatembauche(DateTime $datembauche)
     {
-        $this->_datembauche = ($datembauche);
+        $this->_datembauche = $datembauche;
     }
     public function setFonction($fonction)
     {
-        $this->_fonction = ($fonction);
+        $this->_fonction = $fonction;
     }
     public function setSalaire($salaire)
     {
-        $this->_salaire = ($salaire);
+        $this->_salaire = $salaire;
     }
     public function setService($service)
     {
-        $this->_service = ($service);
-    }
-    public function setDate(DateTime $date)
-    {
-        $this->_date = $date;
+        $this->_service = $service;
     }
 
 
+    // on intialise le constructeur
     public function __construct(array $liste = [])
     {
         if (!empty($liste)) // empty : renvoi vrai si le tableau est vide
@@ -80,8 +75,7 @@ class Employe
 
     public function hydrate($liste)
     {
-        foreach ($liste as $key => $value)
-        {
+        foreach ($liste as $key => $value) {
             $methode = "set" . ucfirst($key); //ucfirst met la 1ere lettre en majuscule
             if (is_callable(([$this, $methode]))) // is_callable verifie que la methode existe
             {
@@ -90,35 +84,36 @@ class Employe
         }
     }
 
+    // on initialise la fonction pour l'affichage
     public function toString()
     {
-        $affichage = "Monsieur/Madame ".$this->getNom()." ".$this->getPrenom()." à été embauché en ".$this->getDatembauche().".\nIl/Elle occupe le poste de ".$this->getFonction()." et son salaire annuel est de ".$this->getSalaire()."€.\nIl/Elle se trouve dans le service ".$this->getService()
-        .".\nCela fait ".$this->ancienneté($this->_datembauche)." ans qu'il(elle) se trouve dans l'entreprise et sa prime annuel est de ".$this->primeAnnuel($this->_salaire)."€.\nSa prime d'ancienneté s'élève à ".$this->primeAncienneté($this->_salaire,$this->ancienneté($this->_datembauche))."€ \n\n";
+        $affichage = "Monsieur/Madame " . $this->getNom() . " " . $this->getPrenom() . " à été embauché en " . $this->getDatembauche()->format('d M Y') . ".\nIl/Elle occupe le poste de " . $this->getFonction() . " et son salaire annuel est de " . $this->getSalaire() . "€.\nIl/Elle se trouve dans le service " . $this->getService()
+        . ".\nCela fait " . $this->ancienneté() . " ans qu'il(elle) se trouve dans l'entreprise et sa prime annuel est de " . $this->primeAnnuel() . "€.\nSa prime d'ancienneté s'élève à " . $this->primeAncienneté($this->_salaire, $this->ancienneté()) . "€ \n\n";
         return $affichage;
-        
+
     }
 
+    // fonction permettant de savoir l'ancienneté d'un employé
     public function ancienneté()
     {
-        // $date = 2020;
-        // $ancien = $date - $datembauche;
-        // return $ancien;
         $date = new DateTime('now');
-        $difference = $date->diff($this->getDatembauche());
+        $ancien = $date->diff($this->getDatembauche());
+        return $ancien->format('%Y')*1;
     }
 
-    public function primeAnnuel($salaire)
+    // fonction permettant de calculer la prime annuel d'un employé
+    private function primeAnnuel()
     {
-        $primeAnnuel = (5/100) * $salaire;
+        $primeAnnuel = (5 / 100) * $this->getSalaire();
         return $primeAnnuel;
     }
 
-    public function primeAncienneté($salaire,$ancien)
+    // fonction permettant de calculer la prime d'ancienneté d'un employé
+    private function primeAncienneté($salaire, $ancien)
     {
-        $primeAnciennete = (2/100) * $salaire * $ancien;
+        $primeAnciennete = (2 / 100) * $salaire * $ancien;
         return $primeAnciennete;
     }
 
 }
 
-?>
--- PHP/Orientee Objet/TestEmploye.php
+++ PHP/Orientee Objet/TestEmploye.php
@@ -13,15 +13,15 @@ require "ClassEmploye.php";
 // $employe5 = new Employe(["nom" => "Fernand", "prenom" => "Christiane", "datembauche" => "2019", "fonction" => "vétérinaire", "salaire" => "2250", "service" => "médical"]);
 // echo $employe5 -> toString();
 
-$employe1 = new Employe(["nom" => "Pavu", "prenom" => "Paprika", "datembauche" => "2014-05-10", "fonction" => "vendeur", "salaire" => "2200", "service" => "commercial"]);
+$employe1 = new Employe(["nom" => "Pavu", "prenom" => "Paprika", "datembauche" => new DateTime('2014-05-10'), "fonction" => "vendeur", "salaire" => "2200", "service" => "commercial"]);
 echo $employe1 -> toString();
-$employe2 = new Employe(["nom" => "Pruvost", "prenom" => "Christian", "datembauche" => "1984", "fonction" => "cariste", "salaire" => "1850", "service" => "transport"]);
+$employe2 = new Employe(["nom" => "Pruvost", "prenom" => "Christian", "datembauche" => new DateTime('1984-07-11'), "fonction" => "cariste", "salaire" => "1850", "service" => "transport"]);
 echo $employe2 -> toString();
-$employe3 = new Employe(["nom" => "Gradis", "prenom" => "Lilian", "datembauche" => "2010", "fonction" => "plombier", "salaire" => "1900", "service" => "commercial"]);
+$employe3 = new Employe(["nom" => "Gradis", "prenom" => "Lilian", "datembauche" => new DateTime('2010-03-05'), "fonction" => "plombier", "salaire" => "1900", "service" => "commercial"]);
 echo $employe3 -> toString();
-$employe4 = new Employe(["nom" => "Poivron", "prenom" => "Alima", "datembauche" => "1999", "fonction" => "medeçin", "salaire" => "2400", "service" => "médical"]);
+$employe4 = new Employe(["nom" => "Poivron", "prenom" => "Alima", "datembauche" => new DateTime('1999-01-23'), "fonction" => "medeçin", "salaire" => "2400", "service" => "médical"]);
 echo $employe4 -> toString();
-$employe5 = new Employe(["nom" => "Fernand", "prenom" => "Christiane", "datembauche" => "2019", "fonction" => "vétérinaire", "salaire" => "2250", "service" => "médical"]);
+$employe5 = new Employe(["nom" => "Fernand", "prenom" => "Léa", "datembauche" => new DateTime('2019-03-08'), "fonction" => "vétérinaire", "salaire" => "2250", "service" => "médical"]);
 echo $employe5 -> toString();
 
 ?>
