462e0316abe4f174df829b73ea6b24a5d3439835
59011-95-06 <59011-95-06@exchange.ad.afpanet>
2020-10-08 14:39:59 +0200

    version0

---
+++ PHP/Orientee Objet/05 ExerciceClassGeometrie/ClassRectangle.php
@@ -0,0 +1,83 @@
+<?php
+
+class Rectangle
+{
+
+    /*****************Attributs***************** */
+    private $_longueur;
+    private $_largeur;
+
+    /*****************Accesseurs***************** */
+
+    public function getLongueur()
+    {
+        return $this->_longueur;
+    }
+
+    public function setLongueur($longueur)
+    {
+        $this->_longueur = $longueur;
+    }
+
+    public function getLargeur()
+    {
+        return $this->_largeur;
+    }
+
+    public function setLargeur($largeur)
+    {
+        $this->_largeur = $largeur;
+    }
+
+    /*****************Constructeur***************** */
+
+    public function __construct(array $options = [])
+    {
+        if (!empty($options)) // empty : renvoi vrai si le tableau est vide
+        {
+            $this->hydrate($options);
+        }
+    }
+    public function hydrate($data)
+    {
+        foreach ($data as $key => $value)
+        {
+            $methode = "set" . ucfirst($key); //ucfirst met la 1ere lettre en majuscule
+            if (is_callable(([$this, $methode]))) // is_callable verifie que la methode existe
+            {
+                $this->$methode($value);
+            }
+        }
+    }
+
+    /*****************Autres Méthodes***************** */
+    
+    public function toString()
+    {
+        $reponse =  "Longueur :" .$this->getLongueur(). " Largeur : " .$this->getLargeur() . " Perimetre : " .$this->perimetre(). " Aire : ".$this->aire();
+        $reponse .= $this->estCarre()?" Il s'agit d'un carré":" Il ne s'agit pas d'un carré";
+        return $reponse;;
+    }
+
+    public function perimetre()
+    {
+        return ($this->getLongueur() + $this->getLargeur())*2;
+    }
+
+    public function aire()
+    {
+        return ($this->getLongueur()* $this->getLargeur());
+    }
+
+    public function estCarre()
+    {
+        return ($this->getLongueur() ==  $this->getLargeur());
+    }
+
+    public function afficherRectangle()
+    {
+
+    }
+    
+}
+?>
---
+++ PHP/Orientee Objet/05 ExerciceClassGeometrie/ClassTriangleRetangle.php
@@ -0,0 +1,60 @@
+<?php
+
+class TriangleRectangle
+{
+
+    /*****************Attributs***************** */
+    private $_base;
+    private $_hauteur;
+
+    /*****************Accesseurs***************** */
+    public function getBase()
+    {
+        return $this->_base;
+    }
+
+    public function setBase($base)
+    {
+        $this->_base = $base;
+    }
+
+    public function getHauteur()
+    {
+        return $this->_hauteur;
+    }
+
+    public function setHauteur($hauteur)
+    {
+        $this->_hauteur = $hauteur;
+    }
+    
+    /*****************Constructeur***************** */
+
+    public function __construct(array $options = [])
+    {
+        if (!empty($options)) // empty : renvoi vrai si le tableau est vide
+        {
+            $this->hydrate($options);
+        }
+    }
+    public function hydrate($data)
+    {
+        foreach ($data as $key => $value)
+        {
+            $methode = "set" . ucfirst($key); //ucfirst met la 1ere lettre en majuscule
+            if (is_callable(([$this, $methode]))) // is_callable verifie que la methode existe
+            {
+                $this->$methode($value);
+            }
+        }
+    }
+
+    /*****************Autres Méthodes***************** */
+    
+    public function toString()
+    {
+        return "";
+    }
+
+}
+?>
