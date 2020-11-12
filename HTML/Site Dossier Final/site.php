ce6d38aea3e259bd87223ebd9f9f7517c3f29043
S-Kirua26 <steevengheeraert26pgmail.com>
2020-11-12 10:22:29 +0100

    Version0

--- HTML/Site Dossier Final/site.php
+++ HTML/Site Dossier Final/site.php
@@ -1,10 +1,11 @@
 <?php
 
-$titrePage = "Musculation";
+$titrePage = "Les mangas";
+$titreHeader = "Tout savoir sur las mangas";
 
  include ("head.php");
-
-
+ include ("header.php");
+//  include ("nav.php");
 
 
 echo '</html>';
---
+++ PHP web/Site Dossier Final/site.php
@@ -0,0 +1,10 @@
+<?php
+
+$titrePage = "Musculation";
+$titreHeader = "Exercices Musculation";
+
+ include ("head.php");
+ include ("header.php");
+
+echo '</html>';
+?>
