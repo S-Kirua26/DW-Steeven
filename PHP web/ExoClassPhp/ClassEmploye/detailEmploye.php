8bb280347c5a064f50e94b0104770f875bcce520
S-Kirua26 <steevengheeraert26pgmail.com>
2020-11-16 14:33:03 +0100

    Version0

--- PHP web/ExoClassPhp/ClassEmploye/detailEmploye.php
+++ PHP web/ExoClassPhp/ClassEmploye/detailEmploye.php
@@ -9,31 +9,42 @@ foreach ($personnes as $unePersonne)
 {
     if ($unePersonne->getId() == $idRecherche)
     {
-        echo '<div class="englobe>
+        echo '<div class="englobe colonne">
             <div class="information">
-                <div class="titreInformation">Informations</div>
-                <div class="texteInformation colonne">
-                    <div class="texte">'.$unePersonne->getDatembauche()->format('Y-m-d').'</div>
+                <div class="bouton">
+                    <div class="titreInformation">Informations</div>
+                </div>
+                <div class="article">
+                    <div class="texteInformation colonne">
+                        <div class="texte">'.$unePersonne->getDatembauche()->format('Y-m-d').'</div>
+                        <div class="texte">'.$unePersonne->getPoste().'</div>
+                        <div class="texte">'.$unePersonne->getSalaire().'</div>
+                    </div>
+                </div>
+            </div>
+            <div class="information">
+                <div class="bouton">
+                    <div class="titreInformation">Agence</div>
+                </div>
+                <div class="article">
+                    <div class="texteInformation colonne">
+                        <div class="texte">'.$unePersonne->getAgence()->getNomAgence().'</div>
+                        <div class="texte">'.$unePersonne->getAgence()->getAdresse().'</div>
+                        <div class="texte">'.$unePersonne->getAgence()->getCodePostal().'</div>
+                        <div class="texte">'.$unePersonne->getAgence()->getRestauration().'</div>
+                    </div>
+                </div>
+            </div>
+            <div class="information">
+                <div class="bouton">
+                    <div class="titreInformation">Poste</div>
+                </div>
+                <div class="article">
+                    <div class="texteInformation colonne">
                     <div class="texte">'.$unePersonne->getPoste().'</div>
-                    <div class="texte">'.$unePersonne->getSalaire().'</div>
                 </div>
-            </div>
-            <div class="information">
-                <div class="titreInformation">Agence</div>
-                <div class="texteInformation colonne">
-                    <div class="texte">'.$unePersonne->getAgence()->getNomAgence().'</div>
-                    <div class="texte">'.$unePersonne->getAgence()->getAdresse().'</div>
-                    <div class="texte">'.$unePersonne->getAgence()->getCodePostal().'</div>
-                    <div class="texte">'.$unePersonne->getAgence()->getRestauration().'</div>
-                </div>
-            </div>
-            <div class="information">
-                <div class="titreInformation">Poste</div>
-                <div class="texteInformation colonne">
-                <div class="texte">'.$unePersonne->getPoste().'</div>
-            </div>
-            </div>
-
-        </div>';
+            </div>'
+        
+        echo '</div>';
     }
 }
