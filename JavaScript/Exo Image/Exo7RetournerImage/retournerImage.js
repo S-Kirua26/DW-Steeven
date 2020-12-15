c8bab18c7cbe2a005cb1c1761cbae7d2c6c3d146
S-Kirua26 <steevengheeraert26pgmail.com>
2020-12-15 17:28:15 +0100

    Version0

---
+++ JavaScript/Exo Image/Exo7RetournerImage/retournerImage.js
@@ -0,0 +1,18 @@
+var image = document.getElementById("image");
+var url = image.getAttribute("src");
+
+image.addEventListener("click", retourner)
+
+function retourner()
+{
+    url = image.getAttribute("src");
+
+    if(url == "recto.jpg")
+    {
+        image.setAttribute("src", "verso.jpg");
+    }
+    else if(url == "verso.jpg")
+    {
+        image.setAttribute("src", "recto.jpg");
+    }
+}
